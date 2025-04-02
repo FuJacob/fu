9e0e39f025314f46ab547e94c8b93542a7171275
Jacob Fu <jjacobfu@gmail.com>
2025-04-02 19:29:31 -0400

    fix image

--- frontend/src/app/components/MessageStuff/Home/HomeMessages.jsx
+++ frontend/src/app/components/MessageStuff/Home/HomeMessages.jsx
@@ -15,7 +15,7 @@ const HomeMessages = () => {
       <GrayMessage order={(counter += timeGap)}>
         <div className="overflow-hidden">
           <img
-            src="avatar1.JPG"
+            src="avatar1.jpg"
             className="w-56 h-56 rounded-3xl object-[0%_10%] object-cover"
           />
         </div>
